ea13a6efaea8e1b98ec198483950c792c50c0c66
yunkwangyou <>
2022-05-09 20:51:09 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, abort, json
+import ssl
+
+app = Flask(__name__)
+
+@app.route('/test',methods = ['POST'])
+def outgoing_webhook(): #Teams -> Flask Server로 Receive 받는 Fucntion
+    if request.methods == 'POST':
+        print(request.json)
+        data = {
+            "type":"message",
+            "text":"Echo."
+        }
+        response = app.response_class(response = json.dumps(data), status. mimetype = 'application/json')
+
+        return response_class
+    else :
+        abort(400)
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host="0.0.0.0",port='50')
